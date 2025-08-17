8a17025c2c2078f0647477b313cc6221bd24f65b
Archit Kumar <architk471@gmail.com>
2025-08-17 19:38:13 +0530

    applied register functionality

--- src/components/pages/LoginReg.jsx
+++ src/components/pages/LoginReg.jsx
@@ -1,7 +1,39 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 
 export function LoginReg() {
+
     const [active, setActive] = useState('login')
+    const [registerData, setRegisterData] = useState({ 'username': '', 'email': '', 'password': '' })
+    const [loginData, setLoginData] = useState({ 'email': '', 'password': '' })
+    const handleRegisterChange = (e) => {
+        setRegisterData({
+            ...registerData,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleLoginChange = (e) => {
+        setLoginData({
+            ...loginData,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await axios.post('http://127.0.0.1:8000/api/register/', registerData)
+            // setRegisterData({ 'username': '', 'email': '', 'password': '' })
+            console.log(res.data)
+            alert('Registerd Successfully')
+            setRegisterData({ 'username': '', 'email': '', 'password': '' })
+           
+            setActive('login')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+   
     return (
         <>
             <div className="h-screen grid grid-cols-12 pt-[62px]">
@@ -18,40 +50,40 @@ export function LoginReg() {
                         {active == 'login'
                             ? <form action="" className="login-form flex flex-col gap-5 p-3 rounded-2xl  bg-gray-200">
                                 <div className="row">
-                                    <span className=" w-full font-semibold">Email</span>
-                                    <input type="email" placeholder="Email" className="border w-full p-2 rounded-md"></input>
+                                    <span className=" w-full font-semibold" >Email</span>
+                                    <input name="email" value={loginData.email} type="email" placeholder="Email" className="border w-full p-2 rounded-md" onChange={handleLoginChange}></input>
                                 </div>
                                 <div className="row">
                                     <span className=" w-full font-semibold">Password</span>
-                                    <input type="password" placeholder="Password" className="border w-full p-2 rounded-md"></input>
+                                    <input name="password" value={loginData.password} type="password" placeholder="Password" className="border w-full p-2 rounded-md" onChange={handleLoginChange}></input>
                                 </div>
                                 <button type="submit" className="w-[30%] mx-auto bg-blue-600 font-semibold text-white p-1 flex items-center justify-center rounded-2xl cursor-pointer">Login</button>
 
 
                             </form>
-                            : <form action="" className="register-form flex flex-col gap-5 p-3 bg-gray-200 rounded-2xl">
+                            : <form onSubmit={handleSubmit} className="register-form flex flex-col gap-5 p-3 bg-gray-200 rounded-2xl">
                                 <div className="row">
-                                    <span className=" w-full font-semibold">Name</span>
-                                    <input type="text" placeholder="Email" className="border w-full p-2 rounded-md"></input>
+                                    <span className=" w-full font-semibold">Username</span>
+                                    <input name="username" value={registerData.username} type="text" placeholder="Username" className="border w-full p-2 rounded-md" onChange={handleRegisterChange}></input>
                                 </div>
                                 <div className="row">
                                     <span className=" w-full font-semibold">Email</span>
-                                    <input type="email" placeholder="Email" className="border w-full p-2 rounded-md"></input>
+                                    <input name="email" value={registerData.email} type="email" placeholder="Email" className="border w-full p-2 rounded-md" onChange={handleRegisterChange}></input>
                                 </div>
                                 <div className="row">
                                     <span className=" w-full font-semibold">Password</span>
-                                    <input type="password" placeholder="Password" className="border w-full p-2 rounded-md"></input>
+                                    <input name="password" type="password" value={registerData.password} placeholder="Password" className="border w-full p-2 rounded-md" onChange={handleRegisterChange}></input>
                                 </div>
-                                <div className="row">
+                                {/* <div className="row">
                                     <span className=" w-full font-semibold">Confirm Password</span>
-                                    <input type="password" placeholder="Password" className="border w-full p-2 rounded-md"></input>
-                                </div>
+                                    <input type="confirm_password" placeholder="Password" className="border w-full p-2 rounded-md" onChange={handleRegisterChange}></input>
+                                </div> */}
                                 <div className="row flex gap-2 items-center">
                                     <input type="checkbox" className=""></input>
                                     <span className=" w-full font-semibold">I agree to Terms & Conditions</span>
                                 </div>
-                                
-                                <button type="submit" className="w-[30%] mx-auto bg-blue-600 font-semibold text-white p-1 flex items-center justify-center rounded-2xl cursor-pointer">Register</button>
+
+                                <button type="submit" className="w-[30%] mx-auto bg-blue-600 font-semibold text-white p-1 flex items-center justify-center rounded-2xl cursor-pointer hover:bg-blue-800">Register</button>
 
 
                             </form>
