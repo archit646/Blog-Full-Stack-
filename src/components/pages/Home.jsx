e4456c7e8db167211dbbc75ba940c2cf29847844
Archit Kumar <architk471@gmail.com>
2025-08-25 23:53:20 +0530

    fixed some issues

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,19 +15,19 @@ export function Home({ posts, category,categories, recent, token }) {
                     <h1 className='text-center text-3xl font-bold'>Trending Posts</h1>
                     <Slider posts={posts} token={token} />
                 </div>
-                <div className='grid grid-cols-12 gap-3'>
-                    <div className='col-span-4 sm:col-span-2 border bg-red-300 p-2'>
+                <div className='grid grid-cols-12 gap-3 '>
+                    <div className='col-span-4 sm:col-span-2 border bg-red-300 p-2 hidden sm:block'>
                         <h1 className='text-center text-xl sm:text-2xl font-bold p-3'>Recent Posts</h1>
 
                         <Sidebar  posts={recent}/>
                     </div>
 
-                    <div className='col-span-8 sm:col-span-10 border bg-red-300 w-full'>
+                    <div className='col-span-12 sm:col-span-10 border bg-red-300 w-full'>
                         <h1 className='text-center text-2xl sm:text-3xl font-bold p-3'>All Posts</h1>
-                        <div className='category flex justify-center gap-3 w-[98%] mx-auto bg-blue-600 p-3'>
-                            <h2 className={`${selectedCate==='all'?'bg-green-700':'bg-amber-600 hover:bg-amber-800'} text-white font-semibold rounded-sm px-2 py-1 cursor-pointer`} onClick={()=>setSelectedCate('all')}>All</h2>
+                        <div className='category flex sm:justify-center gap-3 w-[98%] mx-auto bg-blue-600 p-3 overflow-auto'>
+                            <span className={`${selectedCate==='all'?'bg-green-700':'bg-amber-600 hover:bg-amber-800'} text-white font-semibold rounded-sm  cursor-pointer px-2 py-1`} onClick={()=>setSelectedCate('all')}>All</span>
                             {categories?.map((item) => {
-                                return <h2 key={item.id} className={`${selectedCate===item.name?'bg-green-700':'bg-amber-600 hover:bg-amber-800'} text-white font-semibold rounded-sm px-2 py-1 cursor-pointer`} onClick={() => setSelectedCate(item.name)}>{item.name}</h2>
+                                return <span key={item.id} className={`${selectedCate===item.name?'bg-green-700':'bg-amber-600 hover:bg-amber-800'} text-white text:sm font-semibold rounded-sm min-h-1 cursor-pointer whitespace-nowrap px-2 py-1`} onClick={() => setSelectedCate(item.name)}>{item.name}</span>
                             })}
                         </div>
 
