57e9058cc29d0f77562bd84497e05ac47074906b
Archit Kumar <architk471@gmail.com>
2025-08-25 23:31:21 +0530

    fixed some issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export function Navbar({ user, setUser }) {
 
     return (
         <div className="relative w-screen">
-            <div className="navbar fixed w-full flex items-center justify-around sm:justify-around bg-amber-400 top-0 z-50">
+            <div className="navbar fixed w-screen flex items-center justify-around sm:justify-around bg-amber-400 top-0 z-50">
 
                 <img className="logo max-w-[60px] max-h-[60px] sm:max-w-[90px] sm:max-h-[90px] scale-[230%] sm:scale-[230%]" src="/mylogo.png" alt="logo" />
 
