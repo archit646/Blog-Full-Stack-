f0f20b1d128f7c2da9ddd381e45b36765d8a421f
Archit Kumar <architk471@gmail.com>
2025-09-01 21:39:23 +0530

    fixed some issues

--- src/components/pages/LoginReg.jsx
+++ src/components/pages/LoginReg.jsx
@@ -86,17 +86,17 @@ export function LoginReg({setUser}) {
    
     return (
         <>
-            <div className="h-screen grid grid-cols-12 pt-[62px]">
-                <div className="col-span-6">
+            <div className="min-h-screen grid grid-cols-12 pt-[62px] pb-3">
+                <div className="col-span-12 sm:col-span-6">
                     <img className=" w-full h-full object-contain" src="https://static.vecteezy.com/system/resources/previews/028/046/128/non_2x/users-access-emails-from-multiple-devices-vector.jpg"></img>
 
                 </div>
-                <div className="col-span-6 flex justify-center items-center flex-col gap-10 bg-white">
+                <div className="col-span-12 sm:col-span-6 flex justify-center items-center flex-col gap-10 bg-white">
                     <div className="options-container flex border-b-1 w-full justify-center pb-1 gap-5 text-black">
                         <button className={`flex  justify-center items-center cursor-pointer font-semibold px-4 py-1 ${active == 'login' ? 'border-b-5 border-black' : 'border-none'}`} onClick={() => setActive('login')}>Login</button>
                         <button className={`flex  justify-center items-center cursor-pointer font-semibold px-4 py-1 ${active == 'register' ? 'border-b-5 border-black' : 'border-none'}`} onClick={() => setActive('register')}>Register</button>
                     </div>
-                    <div className="form-container w-[70%] h-[80%] p-3  rounded-2xl ">
+                    <div className="form-container  sm:w-[70%] h-[80%] p-3  rounded-2xl ">
                         {active == 'login'
                             ? <form onSubmit={handleLoginSubmit} className="login-form flex flex-col gap-5 p-3 rounded-2xl  bg-gray-200">
                                 <div className="row">
