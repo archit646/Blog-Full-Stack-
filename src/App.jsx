fa9b607f9cf9f059890e19f41cba723dc9789561
Archit Kumar <architk471@gmail.com>
2025-09-01 18:49:05 +0530

    issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
 
 
   return (
-    <div className='w-full'>
+    <div className='max-w-screen'>
       <Navbar user={user} setUser={setUser} />
       <Routes>
         <Route path='/' element={<Home token={token} posts={posts} recent={recent} categories={categories} setCategories={setCategories} category={category} />}></Route>
